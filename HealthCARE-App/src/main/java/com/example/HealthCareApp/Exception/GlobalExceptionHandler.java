1a7da10c58f8c2dd2d9ab52abbd59b2e87afb833
mrxSAW <arkkass2018@gmail.com>
2026-04-28 15:31:32 +0100

    reviews and adds

--- HealthCARE-App/src/main/java/com/example/HealthCareApp/Exception/GlobalExceptionHandler.java
+++ HealthCARE-App/src/main/java/com/example/HealthCareApp/Exception/GlobalExceptionHandler.java
@@ -2,24 +2,27 @@ package com.example.HealthCareApp.Exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
 
         Map<String, String> errors = new HashMap<>();
 
-        ex.getBindingResult().getFieldErrors().forEach(error -> {
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+
+        for (FieldError error : fieldErrors) {
             errors.put(error.getField(), error.getDefaultMessage());
-        });
+        }
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
